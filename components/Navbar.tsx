91fa3178169ce9ba5791e150d9ddfa94dd1c0c66
The-0nly-0n3 <jean.poubelle95@gmail.com>
2023-08-08 14:55:47 +0200

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,87 @@
+"use client" // this is a client component
+import React from "react"
+import { useState } from "react"
+import { Link } from "react-scroll/modules"
+import { usePathname } from "next/navigation"
+import { useTheme } from "next-themes"
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
+
+interface NavItem {
+    label:string
+    page: string
+
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+
+    {
+        label: "Home",
+        page: "home",
+
+
+    },
+    {
+        label: "About",
+        page: "about",
+
+
+    },
+    {
+        label: "Projects",
+        page: "projects",
+
+
+    },
+
+
+]
+
+const Navbar = () => {
+    const {systemTheme, theme, setTheme} = useTheme()
+    const currentTheme = theme === "system" ? systemTheme: theme
+    const [navbar, setNavbar] = useState(false)
+  
+    return (
+    <header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
+        <div className="justify-between md:items-center md:flex">
+            <div>
+                <div className="md:py-5 md:block">
+                    <h2 className="text-2xl font-bold">Eric SCHALK</h2>
+                </div>
+            </div>
+            <div className="md:flex md:space-x-6">
+                {NAV_ITEMS.map((item, idx) => {
+                    return <a key={idx}>{item.label}</a>
+
+                })}
+                    {
+                        currentTheme === "dark" ? (
+                            <button 
+                            onClick={()=>setTheme("light")}
+                             className="bg-state-100 p-2 rounded-xl">
+                                <RiSunLine/>
+                            </button>
+                        ) : (
+                            <button
+                             className="bg-state-100 p-2 rounded-xl"
+                             onClick={()=>setTheme("dark")}
+                             >
+                                
+                                <RiMoonFill />
+                            </button>
+                            
+                            
+                        )
+                        
+                    }
+            </div>
+
+        </div>
+        
+        Navbar</header>
+        
+  )
+}
+
+export default Navbar
